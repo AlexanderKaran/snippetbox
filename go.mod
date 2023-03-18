725d717379211cf939310ba8336997ffd285f468
Alexander Karan <alexanderkaran@hey.com>
2023-03-18 16:42:39 +0800

    Init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.alexanderkaran.com
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(writer http.ResponseWriter, req *http.Request) {
+
+	if req.URL.Path != "/" {
+		http.NotFound(writer, req)
+		return
+	}
+
+	writer.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
