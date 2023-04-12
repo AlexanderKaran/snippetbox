c6ab48a874e555d888da62fbad78657c1bfc60fd
Alexander Karan <alexanderkaran@hey.com>
2023-04-12 18:00:39 +0800

    Added SQL Database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"snippetbox.alexanderkaran.com/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
+	// NOTE: Handle error
+	godotenv.Load()
+
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	password := os.Getenv("DB_PASSWORD")
+	connection := fmt.Sprintf("web:%s@/snippetbox?parseTime=true", password)
+	dsn := flag.String("dsn", connection, "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	server := &http.Server{
@@ -31,8 +54,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
 }
 
-// Chapter 2.9
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
